33d7313f501635d57468c4e04d82c889076b7375
BrnaVigil <vigilbernard45@gmail.com>
2023-09-23 11:10:13 -0600

    agrege etiquetas php

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ejemplo de PHP</title>
+</head>
+<body>
+    <h1>Programando en PHP</h1>
+
+    <?php
+    // Tu código PHP va aquí
+    $mensaje = "¡Hola, mundo! ";
+    echo $mensaje;
+
+    print('hola mundo')
+    ?>
+    <p>Esto es HTML fuera de las etiquetas PHP.</p>
+</body>
+</html>
